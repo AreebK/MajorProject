074e7472b63ec9cbd63b5569bf332198a86d36b2
Areeb Khan <43014295+AreebK@users.noreply.github.com>
2019-01-16 02:06:17 -0600

    Pushes from Jan 7th to Jan 16th
    Forgot to push most days and was mainly working on another project that would help with this being collision detection with bullets which i tried for a week and didnt work out so i tried and am still having troubles. NEED TOO ADD RANDOM SPAWNING AND BULLET DETECTION

--- majorProject/sketch.js
+++ majorProject/sketch.js
@@ -2,160 +2,385 @@
 // Areeb Khan
 // 14/12/2018
 //
+// Source Code for Easy Ai https://gamedev.stackexchange.com/questions/50978/moving-a-sprite-towards-an-x-and-y-coordinate
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-// class Bullet {
-//   constructor(x, y, dx, dy, theImage) {
-//     this.x = x;
-//     this.y = y;
-//     this.dx = dx;
-//     this.dy = dy;
-//     this.radius = 5;
-//     this.offScreen = false;
-//     this.imageToDisplay = theImage;
-//   }
-//
-//   update() {
-//     this.x += this.dx;
-//     this.y += this.dy;
-//     if (this.x >= width + this.radius || this.x <= 0 - this.radius || this.y >= height + this.radius || this.y <= 0 - this.radius) {
-//       this.offScreen = true;
-//     }
-//   }
-//
-//   display() {
-//     // fill(0);
-//     // ellipse(this.x, this.y, this.radius, this.radius);
-//     imageMode(CENTER);
-//     image(this.imageToDisplay, this.x, this.y, 10, 22);
-//   }
-// }
-
-class Player1 {
-  constructor(x, y, theImage) {
+class Bullet {
+  constructor(x, y, dx, dy, theImage) {
     this.x = x;
     this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = 5;
+    this.offScreen = false;
     this.imageToDisplay = theImage;
-    this.dx = 5;
-    this.dy = 5;
-    this.w = 50;
-    this.h = 70;
-    this.bulletArray = [];
-    this.isMovingUp = false;
-    this.isMovingDown = false;
-    this.isMovingRight = false;
-    this.isMovingLeft = false;
   }
 
-  rotatesPlayer() {
-    let dx = mouseX - this.x;
-    let dy = mouseY - this.y;
-    let angle = atan2(dy, dx);
-    rotate(angle);
+  update() {
+    this.x += this.dx;
+    this.y += this.dy;
+    if (this.x >= width + this.radius || this.x <= 0 - this.radius || this.y >= height + this.radius || this.y <= 0 - this.radius) {
+      this.offScreen = true;
+    }
+  }
+  display() {
+  fill(0);
+  rect(this.x, this.y, this.radius, this.radius);
+  imageMode(CENTER);
+  image(this.imageToDisplay, this.x, this.y);
+  }
+}
+
+class Player1 {
+  constructor(x, y, downImg, upImg, leftImg, rightImg, down_idle_img) {
+    this.x = x;
+    this.y = y;
+    this.dx = 5;
+    this.dy = 5;
+    this.bulletArray = [];
+    this.transparency = 0;
+    this.displayDownImg = downImg;
+    this.displayUpImg = upImg;
+    this.displayLeftImg = leftImg;
+    this.displayRightImg = rightImg;
+    this.idleImgDisplay = down_idle_img;
+    this.w = this.idleImgDisplay.width;
+    this.h = this.idleImgDisplay.height;
+    this.isRight = false;
+    this.isLeft = false;
+    this.isUp = false;
+    this.isDown = false;
+    this.isIdle = true;
+    this.shootDown = false;
+    this.shootUp = false;
+    this.shootLeft = false;
+    this.shootRight = false;
   }
 
   handleKeyPress() {
     if (key === "w" || key === "W") {
-      this.isMovingUp = true;
-    }
-    if (key === "s" || key === "S") {
-      this.isMovingDown = true;
+      this.isUp = true;
+      this.isIdle = false;
     }
     if (key === "a" || key === "A") {
-      this.isMovingLeft = true;
+      this.isLeft = true;
+      this.isIdle = false;
+    }
+    if (key === "s" || key === "S") {
+      this.isDown = true;
+      this.isIdle = false;
     }
     if (key === "d" || key === "D") {
-      this.isMovingRight = true;
+      this.isRight = true;
+      this.isIdle = false;
     }
-  //   if (key === " ") {
-  //     //fire photons!!!
-  //     let someBullet = new Bullet(this.x, this.y, 0, -10, bulletImg);
-  //     this.bulletArray.push(someBullet);
-  //   }
+    if (key === "i" || key === "I") {
+      someBullet = new Bullet(this.x, this.y, 0, -10, bulletImgUp);
+      this.bulletArray.push(someBullet);
+      this.shootUp = true;
+      this.shootLeft = false;
+      this.shootRight = false;
+      this.shootDown = false;
+      this.isIdle = false;
+    }
+    if (key === "j" || key === "J") {
+      someBullet = new Bullet(this.x, this.y, -10, 0, bulletImgLeft);
+      this.bulletArray.push(someBullet);
+      this.shootUp = false;
+      this.shootLeft = true;
+      this.shootRight = false;
+      this.shootDown = false;
+      this.isIdle = false;
+    }
+    if (key === "k" || key === "K") {
+      someBullet = new Bullet(this.x, this.y, 0, 10, bulletImgDown);
+      this.bulletArray.push(someBullet);
+      this.shootUp = false;
+      this.shootLeft = false;
+      this.shootRight = false;
+      this.shootDown = true;
+      this.isIdle = false;
+    }
+    if (key === "l" || key === "L") {
+      someBullet = new Bullet(this.x, this.y, 10, 0, bulletImgRight);
+      this.bulletArray.push(someBullet);
+      this.shootUp = false;
+      this.shootLeft = false;
+      this.shootRight = true;
+      this.shootDown = false;
+      this.isIdle = false;
+    }
   }
 
   handleKeyRelease() {
     if (key === "w" || key === "W") {
-      this.isMovingUp = false;
-    }
-    if (key === "s" || key === "S") {
-      this.isMovingDown = false;
+      this.isUp = false;
+      this.isIdle = true;
     }
     if (key === "a" || key === "A") {
-      this.isMovingLeft = false;
+      this.isLeft = false;
+      this.isIdle = true;
+    }
+    if (key === "s" || key === "S") {
+      this.isDown = false;
+      this.isIdle = true;
     }
     if (key === "d" || key === "D") {
-      this.isMovingRight = false;
+      this.isRight = false;
+      this.isIdle = true;
     }
   }
-
   update() {
-    // Updates / Moves the player up and down
-    if (this.isMovingDown) {
-      this.y += this.dy;
-    }
-    if (this.isMovingUp) {
-      this.y -= this.dy;
-    }
-    if (this.isMovingRight) {
+    if (this.isRight) {
       this.x += this.dx;
     }
-    if (this.isMovingLeft) {
+    if (this.isLeft) {
       this.x -= this.dx;
     }
-
-    // // Lets you see the bullets
-    // for (let i = this.bulletArray.length - 1; i >= 0; i--) {
-    //   this.bulletArray[i].update();
-    //   this.bulletArray[i].display();
-    //   if (this.bulletArray[i].offScreen) {
-    //     this.bulletArray.splice(i, 1);
-    //   }
-    // }
+    if (this.isUp) {
+      this.y -= this.dy;
+    }
+    if (this.isDown) {
+      this.y += this.dy;
+    }
+    if (this.x >= 592){
+      this.x -= this.dx;
+    }
+    if (this.x <= 8){
+      this.x += this.dx;
+    }
+    if (this.y >= 592){
+      this.y -= this.dy;
+    }
+    if (this.y <= 8){
+      this.y += this.dy;
+    }
+    //show bullets
+    for (let i = this.bulletArray.length - 1; i >= 0; i--) {
+      this.bulletArray[i].update();
+      this.bulletArray[i].display();
+      if (this.bulletArray[i].offScreen) {
+        this.bulletArray.splice(i, 1);
+      }
+    }
   }
 
   display() {
+    rectMode(CENTER);
+    noStroke();
+    fill(255, 255, 255, this.transparency);
     imageMode(CENTER);
-    push();
-    translate(this.x, this.y);
-    this.rotatesPlayer();
-    image(this.imageToDisplay, 0, 0 + this.h / 2, this.w, this.h);
-    pop();
+    rect(this.x, this.y, this.w, this.h);
+    if (this.isIdle) {
+    image(this.idleImgDisplay, this.x, this.y);
+    }
+    if (this.isRight) {
+      image(this.displayRightImg, this.x, this.y);
+    }
+    if (this.isLeft) {
+      image(this.displayLeftImg, this.x, this.y);
+    }
+    if (this.isUp) {
+      image(this.displayUpImg, this.x, this.y);
+    }
+    if (this.isDown) {
+      image(this.displayDownImg, this.x, this.y);
+    }
+    if (this.shootUp){
+      image(this.displayUpImg, this.x, this.y);
+      this.shootUp = false;
+      this.isIdle = true;
+    }
+    if (this.shootDown){
+      image(this.displayDownImg, this.x, this.y);
+      this.shootDown = false;
+      this.isIdle = true;
+    }
+    if (this.shootLeft){
+      image(this.displayLeftImg, this.x, this.y);
+      this.shootLeft = false;
+      this.isIdle = true;
+    }
+    if (this.shootRight){
+      image(this.displayRightImg, this.x, this.y);
+      this.shootRight = false;
+      this.isIdle = true;
+    }
+  }
+}
+class Slime {
+  constructor(x, y, slimeImage){
+    this.x = x;
+    this.y = y;
+    this.transparency = 0;
+    this.displayIdle = slimeImage;
+    // this.displaySlimeUp = slimeImgUp;
+    // this.displaySlimeLeft = slimeImgLeft;
+    // this.displaySlimeRight = slimeImgRight;
+    //Easy Ai
+    this.glide = 0.01;
+    this.w = this.displayIdle.width;
+    this.h = this.displayIdle.height;
+  }
+  spawn(){
+    this.x = 0;
+    this.y = 300;
   }
 
+
+  update(){
+    let targetX = playerOne.x;
+    let dx = targetX - this.x;
+    this.x += dx * this.glide;
+
+
+    let targetY = playerOne.y;
+    let dy = targetY - this.y;
+    this.y += dy * this.glide;
+
+
+
+  }
+  display(){
+    rectMode(CENTER);
+    noStroke();
+    fill(255, 255, 255, this.transparency);
+    rect(this.x, this.y, this.w, this.h);
+    imageMode(CENTER);
+    image(this.displayIdle, this.x, this.y);
+  }
 }
 
-let grid = [];
-let areeb, playerimg, bulletImg, backgroundImg;
-let state;
+let someBullet;
+
+let grid = [
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+];
+
+let cellSize;
+let rows = 25;
+let cols = 25;
+let bulletImgRight, bulletImgLeft, bulletImgUp, bulletImgDown;
+let grassimg;
+let playerOne;
+let playerUp, playerDown, playerLeft, playerRight;
+let enemySlime;
+let slimeUp, slimeDown, slimeLeft, slimeRight;
+let lifes = 25;
+let lifeHit = false;
+let bulletHit = false;
+
 
 function preload() {
-  playerimg = loadImage("assets/playerIMG.png");
-  bulletImg = loadImage("assets/Just_A_Bullet.png");
-  backgroundImg = loadImage("assets/backgorund.jpg");
+  //Preloads Images for the game
+  //Images for the grid
+  grassImg = loadImage("assets/grass.png");
+  //Images for Character and Enemy
+  playerUp = loadImage("assets/charup.png");
+  playerDown = loadImage("assets/charidle.png");
+  playerLeft = loadImage("assets/charleft.png");
+  playerRight = loadImage("assets/charright.png");
+
+  slimeUp = loadImage("assets/enemyback.png");
+  slimeDown = loadImage("assets/enemyidle_front.png");
+  slimeLeft = loadImage("assets/enemyleft.png");
+  slimeRight = loadImage("assets/enemyright.png");
+
+  //Images for the Bullets
+  bulletImgRight = loadImage("assets/bulletRight.png");
+  bulletImgLeft = loadImage("assets/bulletLeft.png");
+  bulletImgUp = loadImage("assets/bulletUp.png");
+  bulletImgDown = loadImage("assets/bulletDown.png");
+
 
 }
 
 function setup() {
-  createCanvas(1600, 790);
-  state = "gameStart";
-  areeb = new Player1(width/2, height/2, playerimg);
-
+  createCanvas(600, 600);
+  //downImg, upImg, leftImg, rightImg, down_idle_img
+  enemySlime = new Slime(width / 2, height / 1.8, slimeDown);
+  enemySlime.spawn();
+  playerOne = new Player1(width / 2, height / 1.8, playerDown, playerUp, playerLeft, playerRight, playerDown);
+  cellSize = 24;
 }
 
-function draw() {
+function drawMap() {
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
 
-  if (state === "gameStart"){
-    areeb.update();
-    areeb.display();
+      if (grid[j][i] === 1) {
+        image(grassImg, i * cellSize, j * cellSize, cellSize, cellSize);
+      }
+      else if (grid[j][i] === 2) {
+        image(grassImg, i * cellSize, j * cellSize, cellSize, cellSize);
+      }
+    }
   }
 }
 
+function draw() {
+  imageMode(CORNER);
+  drawMap();
+  // Updates and Displays Player One Sprite
+  if (lifes > 0) {
+  playerOne.display();
+  playerOne.update();
+  }
+
+  // Creates one Slime (this is a beta until the actual code for the random generation is created)
+  enemySlime.display();
+  enemySlime.update();
+
+  // bulletHitDetection();
+  hitDetection();
+  //  Basic Detection where you get 3 lives and once at 0 console displays Game Over
+  if (lifeHit) {
+    if (lifes === 0){
+        console.log("Game Over");
+    }
+    lifes -= 1;
+  }
+}
+
+function hitDetection(){
+  lifeHit = collideRectRect(playerOne.x, playerOne.y, playerOne.w, playerOne.h, enemySlime.x, enemySlime.y, enemySlime.w, enemySlime.h);
+  console.log(lifeHit);
+}
+
+function bulletHitDetection(){
+  bulletHit = collideRectRect(enemySlime.x, enemySlime.y, enemySlime.w, enemySlime.h, someBullet.x, someBullet.y, someBullet.radius, someBullet.radius);
+  console.log(bulletHit);
+}
+
 function keyPressed() {
-  areeb.handleKeyPress();
+  playerOne.handleKeyPress();
 }
 
 function keyReleased() {
-  areeb.handleKeyRelease();
+  playerOne.handleKeyRelease();
 }
